4dfa89f69ee7f09d6e7c092198d95665378f63fe
gabfranca <gabriel_franca@outlook.com.br>
2019-10-09 16:34:42 -0300

    first commit

---
+++ Data/StoreDataContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Catalog.Data.Maps;
+using Catalog.Models;
+
+namespace Catalog.Data
+{
+    public class StoreDataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=LAPTOP-3TPUAP15\SQLEXPRESS;Database=pdtctl;Trusted_Connection=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new ProductMap());
+            builder.ApplyConfiguration(new CategoryMap());
+        }
+    }
+}
---
+++ ViewModels/ResultViewModel.cs
@@ -0,0 +1,9 @@
+namespace Catalog.ViewModels.ProductViewModels
+{
+    public class ResultViewModel
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+    }
+}
